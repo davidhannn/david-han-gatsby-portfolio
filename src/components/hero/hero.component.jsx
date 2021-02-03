47a32ae2e7ac1d68cf2416067bce9cdc9c2d3124
David Han <davidhannn@yahoo.com>
2021-02-02 22:00:57 -0800

    added scroll mouse animation

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -21,6 +21,7 @@ const Hero = () => {
                 <h1 data-aos="fade-up" data-aos-duration="1000"><span>David Han</span></h1>
                 <span class="hero-container-description" data-aos="fade-up" data-aos-duration="1500">Web Developer</span>
                 <StyledButton data-aos="fade-up" data-aos-duration="2000">Resume</StyledButton>
+                <div className="icon-scroll"></div>
         </section>
     )
 }
--- src/components/styled-button/styled-button.component.jsx
+++ src/components/styled-button/styled-button.component.jsx
@@ -4,7 +4,7 @@ import './styled-button.styles.scss';
 
 const StyledButton = ({children, ...props}) => {
     return (
-        <a className="styled-button" href="#">
+        <a className="styled-button" href="https://docs.google.com/document/d/e/2PACX-1vRPjKqA_q94biat6eJhqnxOyB6MURLe9Vvjiv_DQm3xaBOU7JChk8ElSsVq4eu6d-9b8UJmpXEfMR3f/pub">
             <span></span>
             <span></span>
             <span></span>
